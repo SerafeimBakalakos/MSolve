ce0d98978da8533fff206262092b374da40fe6bf
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2016-10-17 10:26:24 +0300

    Added classes to return the expectation (probabilitity) that each individual will be selected for reproduction in a GA
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.Analyzers/Optimization/Algorithms/Metaheuristics/GeneticAlgorithms/Selections/Expectations/IExpectationStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Analyzers.Optimization.Algorithms.Metaheuristics.GeneticAlgorithms.Selections.Expectations
+{
+    public interface IExpectationStrategy<T>
+    {
+        /// <summary>
+        /// Computes the expectation of each <see cref="Individual{T}"/>. They must be positive numbers. 
+        /// These expectations will serve as the probability that each <see cref="Individual{T}"/> 
+        /// will be selected for reproduction.
+        /// </summary>
+        /// <param name="individuals"></param>
+        /// <returns>A vector with the expectations</returns>
+        double[] CalculateExpectations(Individual<T>[] individuals);
+    }
+}
