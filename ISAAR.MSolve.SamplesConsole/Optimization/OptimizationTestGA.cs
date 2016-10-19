0390858d5316e555af3543ff13dc141288f3917f
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2016-10-19 14:35:31 +0300

    New GA selection strategy: Truncation
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- ISAAR.MSolve.SamplesConsole/Optimization/OptimizationTestGA.cs
+++ ISAAR.MSolve.SamplesConsole/Optimization/OptimizationTestGA.cs
@@ -31,7 +31,8 @@ namespace ISAAR.MSolve.SamplesConsole.Optimization
             //optimBuilder.Selection = new RouletteWheelSelection<bool>(new InverseRankScaling<bool>(0.5));
             //optimBuilder.Selection = new TournamentSelection<bool>(2, false);
             //optimBuilder.Selection = new StochasticUniversalSampling<bool>(new InverseRankScaling<bool>(2.0
-            optimBuilder.Selection = new RemainderStochasticSamplingWithReplacement<bool>(new InverseRankScaling<bool>(1.0));
+            //optimBuilder.Selection = new RemainderStochasticSamplingWithReplacement<bool>(new InverseRankScaling<bool>(1.0));
+            optimBuilder.Selection = new TruncationSelection<bool>(0.5);
             optimBuilder.Recombination = new SinglePointCrossover<bool>();
             optimBuilder.Mutation = new BitFlipMutation(0.05);
 
