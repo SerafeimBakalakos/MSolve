c85dfb96ed9082250ad1f608e988332dc2242e9d
Manolis Georgioudakis <geoem@mail.ntua.gr>
2016-11-02 20:44:47 +0200

    Add DifferentialEvolutionAlgorithmConstrained to solve constrained optimization problems with penalty functions.
    Signed-off-by: Manolis Georgioudakis <geoem@mail.ntua.gr>

--- ISAAR.MSolve.Analyzers/Optimization/Algorithms/Metaheuristics/DifferentialEvolution/DifferentialEvolutionAlgorithmConstrained.cs
+++ ISAAR.MSolve.Analyzers/Optimization/Algorithms/Metaheuristics/DifferentialEvolution/DifferentialEvolutionAlgorithmConstrained.cs
@@ -1,57 +1,260 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ISAAR.MSolve.Analyzers.Optimization.Constraints.Penalties;
+﻿using static ISAAR.MSolve.Analyzers.Optimization.Commons.VectorOperations;
 using ISAAR.MSolve.Analyzers.Optimization.Convergence;
+using System;
 using ISAAR.MSolve.Analyzers.Optimization.Problem;
+using ISAAR.MSolve.Analyzers.Optimization.Constraints.Penalties;
 
 namespace ISAAR.MSolve.Analyzers.Optimization.Algorithms.Metaheuristics.DifferentialEvolution
 {
-    public class DifferentialEvolutionAlgorithmConstrained: DifferentialEvolutionAlgorithm
+    /// <summary>
+    ///     Class implementing the Differential Evolution metaheuristic search algorithm
+    ///     <see href = " https://doi.org/10.1023/A:1008202821328"> 
+    ///     R. Storn and K. Price (1997). "Differential evolution - a simple and efficient heuristic 
+    ///     for global optimization over continuous spaces", Journal of Global Optimization, 11, pp.341–359.
+    ///     </see>
+    /// </summary>
+    public class DifferentialEvolutionAlgorithmConstrained : IOptimizationAlgorithm
     {
-        private IConstraintFunction[] inequalityConstraints;
+        // Differential evolution parameters
+        protected readonly int populationSize = 40;
+        private double mutationFactor = 0.6;
+        private double crossoverProbability = 0.9;
+
+        private IConvergenceCriterion convergenceCriterion;
+        private readonly Random randomNumberGenerator = new Random();
+
+        // Optimization problem definition
+        private readonly int dimension;
+        protected IDesignFactory designFactory;
+        private double[] lowerBound;
+        private double[] upperBound;
         private IPenaltyStatic penalty;
 
-        protected DifferentialEvolutionAlgorithmConstrained(OptimizationProblem optimizationProblem, int populationSize,
-                                double mutationFactor, double crossoverProbability, IConvergenceCriterion convergenceCriterion,
-                                IPenaltyStatic penalty):
-            base (optimizationProblem, populationSize, mutationFactor, crossoverProbability, convergenceCriterion)
+        private Individual[] individuals;
+        private Individual[] offsprings;
+
+        protected DifferentialEvolutionAlgorithmConstrained(DesignProblem optimizationProblem, int populationSize,
+            double mutationFactor, double crossoverProbability, IConvergenceCriterion convergenceCriterion, IPenaltyStatic penalty)
         {
-            this.inequalityConstraints = optimizationProblem.InequalityConstraints;
+            this.dimension = optimizationProblem.Dimension;
+            this.lowerBound = optimizationProblem.LowerBound;
+            this.upperBound = optimizationProblem.UpperBound;
+            this.designFactory = optimizationProblem.DesignFactory;
+
+            this.populationSize = populationSize;
+            this.mutationFactor = mutationFactor;
+            this.crossoverProbability = crossoverProbability;
+            this.convergenceCriterion = convergenceCriterion;
             this.penalty = penalty;
         }
 
-        protected override void Evaluation(Individual[] individuals)
+        public double BestFitness
         {
-            //base.Evaluation(individuals);
+            get; private set;
+        } = double.MaxValue;
+
+        public double[] BestPosition
+        {
+            get; private set;
+        }
+
+        public int CurrentIteration
+        {
+            get; private set;
+        }
+
+        public double CurrentFunctionEvaluations
+        {
+            get; protected set;
+        }
+
+        private void Initialize()
+        {
+            individuals = new Individual[populationSize];
 
             for (int i = 0; i < populationSize; i++)
             {
-                double fitness = objectiveFunction.Evaluate(individuals[i].Position);
-                individuals[i].ObjectiveValue = penalty.Evaluate(fitness, individuals[i].Position);
+                double[] position = new double[dimension];
+
+                for (int v = 0; v < dimension; v++)
+                {
+                    position[v] = lowerBound[v] + randomNumberGenerator.NextDouble() * (upperBound[v] - lowerBound[v]);
+                }
+
+                individuals[i] = new Individual(position, double.MaxValue);
+            }
+
+            // Evaluate the initial population
+            Evaluation(individuals);
+
+            // Store the best fitness and position
+            for (int i = 0; i < populationSize; i++)
+            {
+                if (individuals[i].ObjectiveValue < BestFitness)
+                {
+                    BestFitness = individuals[i].ObjectiveValue;
+                    BestPosition = individuals[i].Position;
+                }
+            }
+        }
+
+        private void Iterate()
+        {
+            Mutation();
+            CheckBounds();
+            Recombination();
+            Evaluation(offsprings);
+            Selection();
+        }
+
+        /// <summary>
+        ///     Performs crossover according to binary operation.
+        /// </summary>
+        private void Recombination()
+        {
+            int jRand = randomNumberGenerator.Next(dimension);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                double[] donorVector = offsprings[i].Position;
+                double[] trialVector = new double[dimension];
+
+                for (int d = 0; d < dimension; d++)
+                {
+                    if (randomNumberGenerator.NextDouble() < crossoverProbability || d == jRand)
+                        trialVector[d] = donorVector[d];
+                    else
+                        trialVector[d] = individuals[i].Position[d];
+
+                }
+                offsprings[i].Position = trialVector;
+            }
+
+        }
+        /// <summary>
+        ///     Performs selection
+        /// </summary>
+
+        private void Selection()
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                if (offsprings[i].ObjectiveValue < individuals[i].ObjectiveValue)
+                {
+                    individuals[i] = offsprings[i];
+                }
+
+                if (individuals[i].ObjectiveValue < BestFitness)
+                {
+                    BestFitness = individuals[i].ObjectiveValue;
+                    BestPosition = individuals[i].Position;
+                }
+            }
+        }
+
+        protected virtual void Evaluation(Individual[] individuals)
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                IDesign design = designFactory.CreateDesign(individuals[i].Position);
+                double fitness = design.ObjectiveValues[0];
+
+                individuals[i].ObjectiveValue = penalty.Evaluate(fitness, design);
+
             }
             CurrentFunctionEvaluations += this.populationSize;
         }
 
-        public new class Builder : DifferentialEvolutionAlgorithm.Builder
+        /// <summary>
+        ///     Performs mutation of the population according to de/rand/1/bin scheme
+        /// </summary>
+        private void Mutation()
         {
-            private OptimizationProblem optimizationProblem;
+            offsprings = new Individual[populationSize];
 
-            public IPenaltyStatic Penalty { get; set; }
+            for (int i = 0; i < populationSize; i++)
+            {
+                int r1 = randomNumberGenerator.Next(populationSize);
+                int r2 = randomNumberGenerator.Next(populationSize);
+                int r3 = randomNumberGenerator.Next(populationSize);
 
-            public Builder(OptimizationProblem optimizationProblem) : base(optimizationProblem)
+
+                double[] donorVector = Add(individuals[r1].Position, Scale(mutationFactor, Subtract(individuals[r2].Position, individuals[r3].Position)));
+
+                offsprings[i] = new Individual(donorVector, double.MaxValue);
+            }
+
+        }
+
+        /// <summary>
+        ///     Verify upper and lower bounds for the donor vector
+        /// </summary>
+        private void CheckBounds()
+        {
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                double[] donorVector = offsprings[i].Position;
+
+                for (int j = 0; j < dimension; j++)
+                {
+                    if (donorVector[j] > upperBound[j]) donorVector[j] = upperBound[j];
+                    else if (donorVector[j] < lowerBound[j]) donorVector[j] = lowerBound[j];
+                }
+            }
+        }
+
+        public void Solve()
+        {
+            Initialize();
+            CurrentIteration = 0;
+            while (!convergenceCriterion.HasConverged(this))
+            {
+                CurrentIteration++;
+                Iterate();
+
+                // Write best fitness and position
+                Console.WriteLine(String.Format(@"Iter: {0} | {1} ", CurrentIteration, BestFitness));
+                //Array.ForEach(BestPosition, x => Console.WriteLine(x));
+            }
+        }
+
+        public class Builder
+        {
+            private readonly DesignProblem optimizationProblem;
+
+            public int PopulationSize
+            {
+                get; set;
+            }
+
+            public double MutationFactor
+            {
+                get; set;
+            }
+
+            public double CrossoverProbability
+            {
+                get; set;
+            }
+
+            public IConvergenceCriterion ConvergenceCriterion
+            {
+                get; set;
+            }
+
+            public Builder(DesignProblem optimizationProblem)
             {
                 this.optimizationProblem = optimizationProblem;
             }
 
-            public new IOptimizationAlgorithm Build()
+            public IPenaltyStatic Penalty { get; set; }
+
+            public IOptimizationAlgorithm Build()
             {
-                ProblemChecker.Check(optimizationProblem);
                 return new DifferentialEvolutionAlgorithmConstrained(optimizationProblem, PopulationSize,
                     MutationFactor, CrossoverProbability, ConvergenceCriterion, Penalty);
             }
         }
-
     }
 }
