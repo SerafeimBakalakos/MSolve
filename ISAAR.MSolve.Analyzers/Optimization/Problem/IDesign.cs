3372b6be7d59dd429e75da3576769d9b93dfac38
Manolis Georgioudakis <geoem@mail.ntua.gr>
2016-11-02 20:42:04 +0200

    Add IDesign interface and DesignProblem class for generalizing the optimization procedure for structural problems.
    Signed-off-by: Manolis Georgioudakis <geoem@mail.ntua.gr>

---
+++ ISAAR.MSolve.Analyzers/Optimization/Problem/IDesign.cs
@@ -0,0 +1,13 @@
+﻿namespace ISAAR.MSolve.Analyzers.Optimization.Problem
+{
+    public interface IDesign
+    {
+        double[] ObjectiveValues { get; }
+        double[] ConstraintValues { get; }
+    }
+
+    public interface IDesignFactory
+    {
+        IDesign CreateDesign(double[] x);
+    }
+}
