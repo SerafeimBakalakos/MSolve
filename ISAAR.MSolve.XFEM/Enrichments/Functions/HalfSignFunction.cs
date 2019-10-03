3e0d10a184683fd1d7b34e880a32c93a70e3750d
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2019-10-03 19:22:20 +0300

    Added enrichment for bimaterial interfaces in thermal problems.

---
+++ ISAAR.MSolve.XFEM/Enrichments/Functions/HalfSignFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISAAR.MSolve.LinearAlgebra.Vectors;
+using ISAAR.MSolve.XFEM.Utilities;
+
+namespace ISAAR.MSolve.XFEM.Enrichments.Functions
+{
+    public class HalfSignFunction : IHeavisideFunction2D
+    {
+        public HalfSignFunction()
+        {
+        }
+
+        public double EvaluateAt(double signedDistance)
+        {
+            if (signedDistance > 0.0) return 0.5;
+            else if (signedDistance < 0.0) return -0.5;
+            else return 0.0;
+        }
+
+        public EvaluatedFunction2D EvaluateAllAt(double signedDistance)
+        {
+            var derivatives = Vector2.Create(0.0, 0.0 );
+            if (signedDistance > 0) return new EvaluatedFunction2D(0.5, derivatives);
+            else if (signedDistance < 0) return new EvaluatedFunction2D(-0.5, derivatives);
+            else return new EvaluatedFunction2D(0.0, derivatives);
+        }
+
+        public override string ToString()
+        {
+            return "0.5*Heaviside";
+        }
+    }
+}
---
+++ ISAAR.MSolve.XFEM/Enrichments/Items/ThermalInterfaceEnrichment.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Discretization.FreedomDegrees;
+using ISAAR.MSolve.Discretization.Integration;
+using ISAAR.MSolve.FEM.Interpolation;
+using ISAAR.MSolve.Geometry.Coordinates;
+using ISAAR.MSolve.Geometry.Shapes;
+using ISAAR.MSolve.LinearAlgebra.Vectors;
+using ISAAR.MSolve.XFEM.CrackGeometry.MaterialInterfaces;
+using ISAAR.MSolve.XFEM.Elements;
+using ISAAR.MSolve.XFEM.Enrichments.Functions;
+using ISAAR.MSolve.XFEM.Entities;
+using ISAAR.MSolve.XFEM.FreedomDegrees;
+using ISAAR.MSolve.XFEM.Utilities;
+
+namespace ISAAR.MSolve.XFEM.Enrichments.Items
+{
+    public class ThermalInterfaceEnrichment
+    {
+        //public enum Subdomain { Positive, Negative, Boundary }
+
+        private readonly HalfSignFunction enrichmentFunction;
+        private HashSet<XThermalElement2D> affectedElements;
+
+        public ThermalInterfaceEnrichment(IMaterialInterface discontinuity)
+        {
+            this.Discontinuity = discontinuity;
+            this.enrichmentFunction = new HalfSignFunction();
+            this.Dofs = new EnrichedDof[] { new EnrichedDof(enrichmentFunction, ThermalDof.Temperature) };
+            this.affectedElements = new HashSet<XThermalElement2D>();
+        }
+
+        public IMaterialInterface Discontinuity { get; }
+
+        public IReadOnlyList<EnrichedDof> Dofs { get; protected set; }
+
+
+        public void EnrichElement(XThermalElement2D element)
+        {
+            if (!affectedElements.Contains(element))
+            {
+                affectedElements.Add(element);
+                element.EnrichmentItems.Add(this);
+            }
+        }
+
+        public double[] EvaluateFunctionsAt(XNode node)
+        {
+            double signedDistance = Discontinuity.SignedDistanceOf(node);
+            return new double[] { enrichmentFunction.EvaluateAt(signedDistance) };
+        }
+
+        public EvaluatedFunction2D[] EvaluateAllAt(IXFiniteElement element,
+             double[] shapeFunctionsAtNaturalPoint)
+        {
+            double signedDistance = Discontinuity.SignedDistanceOf(element, shapeFunctionsAtNaturalPoint);
+            return new EvaluatedFunction2D[] { enrichmentFunction.EvaluateAllAt(signedDistance) };
+        }
+
+        //// TODO: add some tolerance when checking around 0. Perhaps all this is not needed though and I could even 
+        //// ignore points on the interface. It certainly needs a better name
+        ///// <summary>
+        ///// Finds the subdomain where the requested cartesian point lies.
+        ///// </summary>
+        ///// <param name="point"></param>
+        ///// <param name="subdomain">The posi</param>
+        ///// <returns></returns>
+        //public Subdomain LocatePoint(CartesianPoint point)
+        //{
+        //    int sign = Math.Sign(Discontinuity.SignedDistanceOf(point));
+        //    if (sign < 0) return Subdomain.Negative;
+        //    else if (sign > 0) return Subdomain.Positive;
+        //    else return Subdomain.Boundary;
+        //}
+
+        public IReadOnlyList<CartesianPoint> IntersectionPointsForIntegration(XContinuumElement2D element)
+            => throw new NotImplementedException();
+
+    }
+}
