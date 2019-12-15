2500e4c874f16c4c74d63ca75bf6c1e9ee8cd602
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2019-12-15 19:26:04 +0200

    Refactored the selectionf FETI-DP corner nodes in XFEM and added an implementation for the case where each process stores all model data.

---
+++ ISAAR.MSolve.XFEM/Solvers/CrackedFetiDPCornerNodesMpiRedundant.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using ISAAR.MSolve.Discretization.Interfaces;
+using ISAAR.MSolve.LinearAlgebra.Distributed;
+using ISAAR.MSolve.XFEM.CrackGeometry;
+
+//TODO: Remove duplication between this and the serial implementation.
+//TODO: It is possible that some previous corner nodes become internal due to the TipAdaptivePartitioner. How to handle this?
+namespace ISAAR.MSolve.XFEM.Solvers
+{
+    /// <summary>
+    /// Assumes all model data are stored in all processes. Each process will only deal with the corner nodes of its 
+    /// corresponding subdomains. Master process will deal with all suubdomain and global data.
+    /// </summary>
+    public class CrackedFetiDPCornerNodesMpiRedundant: CrackedFetiDPCornerNodesBase
+    {
+        private const int cornerNodesTag = 0;
+
+        private readonly IModel model;
+        private readonly ProcessDistribution procs;
+
+        public CrackedFetiDPCornerNodesMpiRedundant(ProcessDistribution processDistribution, IModel model, 
+            ICrackDescription crack, Func<ISubdomain, HashSet<INode>> getInitialCornerNodes) :
+            base(crack, getInitialCornerNodes)
+        {
+            this.procs = processDistribution;
+            this.model = model;
+        }
+
+        public bool AreGlobalCornerNodesModified
+        {
+            get
+            {
+                procs.CheckProcessIsMaster();
+                return areGlobalCornerNodesModified;
+            }
+        }
+
+        public override HashSet<INode> GlobalCornerNodes
+        {
+            get
+            {
+                procs.CheckProcessIsMaster();
+                return cornerNodesGlobal;
+            }
+        }
+
+        public bool AreCornerNodesOfSubdomainModified(ISubdomain subdomain)
+        {
+            procs.CheckProcessMatchesSubdomainUnlessMaster(subdomain.ID);
+            return areCornerNodesModified[subdomain];
+        }
+
+        public override HashSet<INode> GetCornerNodesOfSubdomain(ISubdomain subdomain)
+        {
+            procs.CheckProcessMatchesSubdomainUnlessMaster(subdomain.ID);
+            return cornerNodesOfSubdomains[subdomain];
+        }
+
+        public override void Update()
+        {
+            if (procs.IsMasterProcess)
+            {
+                base.UpdateSubdomainsCorners(model.EnumerateSubdomains());
+                base.GatherGlobalCornerNodes();
+            }
+            else
+            {
+                int[] subdomainIDs = procs.GetSubdomainIdsOfProcess(procs.OwnRank);
+                IEnumerable<ISubdomain> subdomainsToUpdate = subdomainIDs.Select(s => model.GetSubdomain(s));
+                base.UpdateSubdomainsCorners(subdomainsToUpdate);
+            }
+            isFirstAnalysis = false;
+            //WriteCornerNodes();
+        }
+
+        private void WriteCornerNodes()
+        {
+            MpiUtilities.DoInTurn(procs.Communicator, () =>
+            {
+                int s = procs.OwnSubdomainID;
+                Console.Write($"Process {procs.OwnRank}: Corner nodes of subdomain {s}: ");
+                foreach (INode node in cornerNodesOfSubdomains[model.GetSubdomain(s)]) Console.Write(node.ID + " ");
+                Console.WriteLine();
+            });
+        }
+    }
+}
--- ISAAR.MSolve.XFEM/Solvers/CrackedFetiDPCornerNodesSerial.cs
+++ ISAAR.MSolve.XFEM/Solvers/CrackedFetiDPCornerNodesSerial.cs
@@ -3,147 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using ISAAR.MSolve.Discretization.Interfaces;
-using ISAAR.MSolve.Geometry.Coordinates;
-using ISAAR.MSolve.Solvers.DomainDecomposition.Dual.FetiDP.CornerNodes;
 using ISAAR.MSolve.XFEM.CrackGeometry;
-using ISAAR.MSolve.XFEM.Elements;
-using ISAAR.MSolve.XFEM.Entities;
 
 //TODO: It is possible that some previous corner nodes become internal due to the TipAdaptivePartitioner. How to handle this?
 namespace ISAAR.MSolve.XFEM.Solvers
 {
-    public class CrackedFetiDPCornerNodesSerial : ICornerNodeSelection
+    public class CrackedFetiDPCornerNodesSerial : CrackedFetiDPCornerNodesBase
     {
-        private readonly ICrackDescription crack;
-        //private readonly Dictionary<int, HashSet<INode>> currentCornerNodes;
-        private readonly Func<ISubdomain, HashSet<INode>> getInitialCornerNodes;
         private readonly IModel model;
 
-        private Dictionary<ISubdomain, bool> areCornerNodesModified;
-        private bool areGlobalCornerNodesModified = true;
-        private HashSet<INode> cornerNodesGlobal;
-        private Dictionary<ISubdomain, HashSet<INode>> cornerNodesOfSubdomains = new Dictionary<ISubdomain, HashSet<INode>>();
-        private bool isFirstAnalysis; //TODO: This should be passed to Update(). Update should not be called by the solver.
-
         public CrackedFetiDPCornerNodesSerial(IModel model, ICrackDescription crack, 
-            Func<ISubdomain, HashSet<INode>> getInitialCornerNodes)
+            Func<ISubdomain, HashSet<INode>> getInitialCornerNodes) : base (crack, getInitialCornerNodes)
         {
             this.model = model;
-            this.crack = crack;
-            this.getInitialCornerNodes = getInitialCornerNodes;
-            this.isFirstAnalysis = true;
         }
 
         public bool AreGlobalCornerNodesModified => areGlobalCornerNodesModified;
 
-        public HashSet<INode> GlobalCornerNodes => cornerNodesGlobal;
+        public override HashSet<INode> GlobalCornerNodes => cornerNodesGlobal;
 
         public bool AreCornerNodesOfSubdomainModified(ISubdomain subdomain) => areCornerNodesModified[subdomain];
 
-        public HashSet<INode> GetCornerNodesOfSubdomain(ISubdomain subdomain) => cornerNodesOfSubdomains[subdomain];
+        public override HashSet<INode> GetCornerNodesOfSubdomain(ISubdomain subdomain) => cornerNodesOfSubdomains[subdomain];
 
-        public void Update()
+        public override void Update()
         {
-            if (isFirstAnalysis)
-            {
-                foreach (ISubdomain subdomain in model.EnumerateSubdomains())
-                {
-                    cornerNodesOfSubdomains[subdomain] = getInitialCornerNodes(subdomain);
-                }
-            }
-
-            // Keep track of subdomains with modified corner nodes.
-            areCornerNodesModified = new Dictionary<ISubdomain, bool>();
-            foreach (ISubdomain subdomain in cornerNodesOfSubdomains.Keys)
-            {
-                areCornerNodesModified[subdomain] = isFirstAnalysis;
-            }
-
-            // Remove the previous corner nodes that are no longer boundary.
-            foreach (ISubdomain subdomain in cornerNodesOfSubdomains.Keys)
-            {
-                int numEntriesRemoved = cornerNodesOfSubdomains[subdomain].RemoveWhere(node => node.Multiplicity < 2);
-                if (numEntriesRemoved > 0) areCornerNodesModified[subdomain] = true;
-            }
-
-            // Remove the previous corner nodes that do not belong to each subdomain
-            foreach (ISubdomain subdomain in cornerNodesOfSubdomains.Keys)
-            {
-                var removedNodes = new HashSet<INode>();
-                foreach (INode node in cornerNodesOfSubdomains[subdomain])
-                {
-                    try
-                    {
-                        subdomain.GetNode(node.ID);
-                    }
-                    catch (KeyNotFoundException)
-                    {
-                        removedNodes.Add(node);
-                    }
-                }
-                foreach (INode node in removedNodes) cornerNodesOfSubdomains[subdomain].Remove(node);
-                if (removedNodes.Count > 0) areCornerNodesModified[subdomain] = true;
-            }
-
-            // Add boundary Heaviside nodes and nodes of the tip element(s).
-            HashSet<XNode> enrichedBoundaryNodes = FindNewEnrichedBoundaryNodes();
-            foreach (XNode node in enrichedBoundaryNodes)
-            {
-                foreach (ISubdomain subdomain in node.SubdomainsDictionary.Values)
-                {
-                    cornerNodesOfSubdomains[subdomain].Add(node);
-                    areCornerNodesModified[subdomain] = true;
-                }
-            }
-
-            GatherGlobalCornerNodes();
+            base.UpdateSubdomainsCorners(model.EnumerateSubdomains());
+            base.GatherGlobalCornerNodes();
             isFirstAnalysis = false;
-        }
-
-        private HashSet<XNode> FindNewEnrichedBoundaryNodes()
-        {
-            var enrichedBoundaryNodes = new HashSet<XNode>();
-            foreach (CartesianPoint crackTip in crack.CrackTips)
-            {
-                foreach (XContinuumElement2D tipElement in crack.CrackTipElements[crackTip])
-                {
-                    foreach (XNode node in tipElement.Nodes)
-                    {
-                        if (node.Multiplicity > 1) enrichedBoundaryNodes.Add(node);
-                    }
-                }
-            }
-            foreach (var crackBodyNewNodes in crack.CrackBodyNodesNew)
-            {
-                foreach (XNode node in crackBodyNewNodes.Value)
-                {
-                    if (node.Multiplicity > 1) enrichedBoundaryNodes.Add(node);
-                }
-            }
-            return enrichedBoundaryNodes;
-        }
-
-        private void GatherGlobalCornerNodes()
-        {
-            // Define them
-            cornerNodesGlobal = new HashSet<INode>();
-            foreach (IEnumerable<INode> subdomainNodes in cornerNodesOfSubdomains.Values)
-            {
-                cornerNodesGlobal.UnionWith(subdomainNodes);
-            }
-
-            // Determine if they are modified
-            areGlobalCornerNodesModified = false;
-            foreach (bool modifiedSubdomain in areCornerNodesModified.Values)
-            {
-                if (modifiedSubdomain)
-                {
-                    areGlobalCornerNodesModified = true;
-                    break;
-                }
-            }
-
-            CornerNodeUtilities.CheckGlobalCornersAreSubdomainCorners(cornerNodesGlobal, s => cornerNodesOfSubdomains[s]);
         }
     }
 }
