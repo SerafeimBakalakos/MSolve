6ae51b64876e7f1d3200ddd68c3d9a94b143b9b6
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2020-01-05 22:29:21 +0200

    Added dedicated class for efficient matrix-vector and matrix-matrix multiplications, where the (first) matrix has only one non zero entry per column. Also added corresponding tests.

--- ISAAR.MSolve.LinearAlgebra/Matrices/Operators/IMappingMatrix.cs
+++ ISAAR.MSolve.LinearAlgebra/Matrices/Operators/IMappingMatrix.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using ISAAR.MSolve.LinearAlgebra.Vectors;
 
+//TODO: What about transpose(this) * other * this operations? More importantly, what about accessing rows2cols (or cols2rows) map? 
 namespace ISAAR.MSolve.LinearAlgebra.Matrices.Operators
 {
     public interface IMappingMatrix : IBounded2D
