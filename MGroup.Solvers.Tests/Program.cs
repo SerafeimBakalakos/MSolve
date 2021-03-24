62c298557e28ab0c29d5385473fa063a2a52c5d9
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-03-24 14:31:08 +0200

    New project for solvers

---
+++ MGroup.Solvers.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MGroup.Solvers.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
