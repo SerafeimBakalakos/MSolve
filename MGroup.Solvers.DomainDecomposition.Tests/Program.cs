690985d8207406df4507e73e269b46e74cf7aac5
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-05-31 21:06:10 +0300

    Test project for DDM solvers

---
+++ MGroup.Solvers.DomainDecomposition.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MGroup.Solvers.DomainDecomposition.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
