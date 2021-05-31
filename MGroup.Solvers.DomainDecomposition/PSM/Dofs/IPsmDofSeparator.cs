d4e5029f0ec3f5af95b8f56ab4a50d449c5b62f3
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-05-31 21:06:22 +0300

    PSM dofs handling

---
+++ MGroup.Solvers.DomainDecomposition/PSM/Dofs/IPsmDofSeparator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Discretization.FreedomDegrees;
+using MGroup.LinearAlgebra.Distributed.Overlapping;
+using MGroup.Solvers.DomainDecomposition.Commons;
+using MGroup.Solvers.DomainDecomposition.Mappings;
+
+//TODOMPI: DofTable should be replaced with an IntTable that stores ids, instead of actual references to nodes and dofs. 
+//		This will make transfering it via MPI much faster.
+//TODO: Naming convention for dofs (free/constrained, boundary/internal/corner/intercluster, subdomain/cluster/global) that will
+//		be followed across all components
+//TODOMPI: I should decouple the code, such that there are classes that define operations on data, classes that define transfer of data,
+//		and classes that define the order of such operations and transfers (sequential, shared parallel or distributed parallel)
+//		and any synchronization (e.g. locks in IDofSeparator classes). This should probably be done for each component 
+//		(e.g. IDofSeparator, IMatrixManager, etc.). However if I can find common code (boilerplate), I could probably avoid some 
+//		duplication. 
+namespace MGroup.Solvers.DomainDecomposition.Psm.Dofs
+{
+	public interface IPsmDofSeparator
+	{
+		DistributedOverlappingIndexer CreateDistributedVectorIndexer();
+
+		void FindCommonDofsBetweenSubdomains();
+
+		DofTable GetSubdomainDofOrderingBoundary(int subdomainID);
+
+		int[] GetSubdomainDofsBoundaryToFree(int subdomainID);
+
+		int[] GetSubdomainDofsInternalToFree(int subdomainID);
+
+		int GetNumSubdomainFreeDofs(int subdomainID);
+
+		void ReorderSubdomainInternalDofs(int subdomainID, DofPermutation dofPermutation);
+
+		void SeparateSubdomainDofsIntoBoundaryInternal();
+	}
+}
---
+++ MGroup.Solvers.DomainDecomposition/PSM/Dofs/PsmDofSeparator.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ISAAR.MSolve.Discretization.FreedomDegrees;
+using ISAAR.MSolve.Discretization.Interfaces;
+using MGroup.Environments;
+using MGroup.LinearAlgebra.Distributed.Overlapping;
+using MGroup.Solvers.Commons;
+using MGroup.Solvers.DomainDecomposition.Commons;
+
+//TODOMPI: Replace DofTable with an equivalent class that uses integers. Also allow clients to choose sorted versions
+//TODOMPI: Instead of going to asking ComputeNode for its neighbors, ISubdomain should contain this info. 
+namespace MGroup.Solvers.DomainDecomposition.Psm.Dofs
+{
+	public class PsmDofSeparator : IPsmDofSeparator
+	{
+		private readonly IComputeEnvironment environment;
+		private readonly IStructuralModel model;
+		private readonly SubdomainTopology subdomainTopology;
+		private readonly ConcurrentDictionary<int, DofTable> subdomainDofOrderingsBoundary = 
+			new ConcurrentDictionary<int, DofTable>();
+		private readonly ConcurrentDictionary<int, int[]> subdomainDofsBoundaryToFree = new ConcurrentDictionary<int, int[]>();
+		private readonly ConcurrentDictionary<int, int[]> subdomainDofsInternalToFree = new ConcurrentDictionary<int, int[]>();
+		private readonly ConcurrentDictionary<int, int> subdomainNumFreeDofs = new ConcurrentDictionary<int, int>();
+
+		//// First key: current subdomain. Second key: neighbor subdomain. Value: dofs at common nodes between these 2 subdomains
+		private readonly Dictionary<int, Dictionary<int, DofSet>> commonDofsBetweenSubdomains
+			= new Dictionary<int, Dictionary<int, DofSet>>();
+
+		public PsmDofSeparator(IComputeEnvironment environment, IStructuralModel model, SubdomainTopology subdomainTopology)
+		{
+			this.environment = environment;
+			this.model = model;
+			this.subdomainTopology = subdomainTopology;
+		}
+
+		public DistributedOverlappingIndexer CreateDistributedVectorIndexer()
+		{
+			var indexer = new DistributedOverlappingIndexer(environment);
+			Action<int> initializeIndexer = subdomainID =>
+			{
+				ISubdomain subdomain = model.GetSubdomain(subdomainID);
+				int numBoundaryDofs = subdomainDofsBoundaryToFree[subdomainID].Length;
+				DofTable boundaryDofs = subdomainDofOrderingsBoundary[subdomainID];
+
+				var allCommonDofIndices = new Dictionary<int, int[]>();
+				foreach (int neighborID in subdomainTopology.GetNeighborsOfSubdomain(subdomainID))
+				{
+					DofSet commonDofs2 = commonDofsBetweenSubdomains[subdomainID][neighborID];
+					var commonDofIndices = new int[commonDofs2.Count()];
+					int idx = 0;
+					foreach ((int nodeID, int dofID) in commonDofs2.EnumerateNodesDofs()) 
+					{
+						//TODO: It would be faster to iterate each node and then its dofs. Same for DofTable. 
+						//		Even better let DofTable take DofSet as argument and return the indices.
+						INode node = model.GetNode(nodeID);
+						IDofType dof = AllDofs.GetDofWithId(dofID);
+						commonDofIndices[idx++] = boundaryDofs[node, dof];
+					}
+					allCommonDofIndices[neighborID] = commonDofIndices;
+				}
+
+				indexer.GetLocalComponent(subdomainID).Initialize(numBoundaryDofs, allCommonDofIndices);
+			};
+			environment.DoPerNode(initializeIndexer);
+			return indexer;
+		}
+
+		public void FindCommonDofsBetweenSubdomains()
+		{
+			// Find all dofs of each subdomain at the common nodes.
+			Action<int> subdomainAction = subdomainID =>
+			{
+				Dictionary<int, DofSet> commonDofs = FindSubdomainDofsAtCommonNodes(subdomainID);
+				commonDofsBetweenSubdomains[subdomainID] = commonDofs;
+			};
+
+			// Send these dofs to the corresponding neighbors and receive theirs.
+			Func<int, AllToAllNodeData<int>> prepareDofsToSend = subdomainID =>
+			{
+				var transferData = new AllToAllNodeData<int>();
+				transferData.sendValues = new ConcurrentDictionary<int, int[]>();
+				foreach (int neighborID in subdomainTopology.GetNeighborsOfSubdomain(subdomainID))
+				{
+					DofSet commonDofs = commonDofsBetweenSubdomains[subdomainID][neighborID];
+
+					//TODOMPI: Serialization & deserialization should be done by the environment, if necessary.
+					transferData.sendValues[neighborID] = commonDofs.Serialize(); 
+				}
+
+				// No buffers for receive values yet, since their lengths are unknown. 
+				// Let the environment create them, by using extra communication.
+				transferData.recvValues = new ConcurrentDictionary<int, int[]>();
+				return transferData;
+			};
+			Dictionary<int, AllToAllNodeData<int>> transferDataPerSubdomain =
+				environment.CreateDictionaryPerNode(prepareDofsToSend);
+			environment.NeighborhoodAllToAll(transferDataPerSubdomain, false);
+
+			// Find the intersection between the dofs of a subdomain and the ones received by its neighbor.
+			Action<int> processReceivedDofs = subdomainID =>
+			{
+				AllToAllNodeData<int> transferData = transferDataPerSubdomain[subdomainID];
+				foreach (int neighborID in subdomainTopology.GetNeighborsOfSubdomain(subdomainID))
+				{
+					DofSet receivedDofs = DofSet.Deserialize(transferData.recvValues[neighborID]);
+					commonDofsBetweenSubdomains[subdomainID][neighborID] =
+						commonDofsBetweenSubdomains[subdomainID][neighborID].IntersectionWith(receivedDofs);
+				}
+			};
+			environment.DoPerNode(processReceivedDofs);
+		}
+
+		public DofTable GetSubdomainDofOrderingBoundary(int subdomainID) => subdomainDofOrderingsBoundary[subdomainID];
+
+		public int[] GetSubdomainDofsBoundaryToFree(int subdomainID) => subdomainDofsBoundaryToFree[subdomainID];
+
+		public int[] GetSubdomainDofsInternalToFree(int subdomainID) => subdomainDofsInternalToFree[subdomainID];
+
+		public int GetNumSubdomainFreeDofs(int subdomainID) => subdomainNumFreeDofs[subdomainID];
+
+		public void ReorderSubdomainInternalDofs(int subdomainID, DofPermutation permutation)
+		{
+			if (permutation.IsBetter)
+			{
+				int[] internalDofs = permutation.ReorderKeysOfDofIndicesMap(subdomainDofsInternalToFree[subdomainID]);
+				subdomainDofsInternalToFree[subdomainID] = internalDofs; 
+			}
+		}
+
+		/// <summary>
+		/// Boundary/internal dofs
+		/// </summary>
+		public void SeparateSubdomainDofsIntoBoundaryInternal()
+		{
+			Action<int> subdomainAction = subdomainID =>
+			{
+				ISubdomain subdomain = model.GetSubdomain(subdomainID);
+				(DofTable boundaryDofOrdering, int[] boundaryToFree, int[] internalToFree) = SeparateSubdomainDofs(subdomain);
+
+				subdomainNumFreeDofs[subdomainID] = subdomain.FreeDofOrdering.NumFreeDofs;
+				subdomainDofOrderingsBoundary[subdomainID] = boundaryDofOrdering;
+				subdomainDofsBoundaryToFree[subdomainID] = boundaryToFree;
+				subdomainDofsInternalToFree[subdomainID] = internalToFree;
+			};
+			environment.DoPerNode(subdomainAction);
+		}
+
+		private Dictionary<int, DofSet> FindSubdomainDofsAtCommonNodes(int subdomainID)
+		{
+			var commonDofsOfSubdomain = new Dictionary<int, DofSet>();
+			ISubdomain subdomain = model.GetSubdomain(subdomainID);
+			DofTable freeDofs = subdomain.FreeDofOrdering.FreeDofs;
+			foreach (int neighborID in subdomainTopology.GetNeighborsOfSubdomain(subdomainID))
+			{
+				var dofSet = new DofSet();
+				foreach (int nodeID in subdomainTopology.GetCommonNodesOfSubdomains(subdomainID, neighborID))
+				{
+					INode node = model.GetNode(nodeID);
+					dofSet.AddDofs(node, freeDofs.GetColumnsOfRow(node));
+				}
+				commonDofsOfSubdomain[neighborID] = dofSet;
+			}
+			return commonDofsOfSubdomain;
+		}
+
+		private static (DofTable boundaryDofOrdering, int[] boundaryToFree, int[] internalToFree) SeparateSubdomainDofs(
+			ISubdomain subdomain)
+		{
+			var boundaryDofOrdering = new DofTable();
+			var boundaryToFree = new List<int>();
+			var internalToFree = new HashSet<int>();
+			int subdomainBoundaryIdx = 0;
+			DofTable freeDofs = subdomain.FreeDofOrdering.FreeDofs;
+			IEnumerable<INode> nodes = freeDofs.GetRows();
+			foreach (INode node in nodes) //TODO: Optimize access: Directly get INode, Dictionary<IDof, int>
+			{
+				IReadOnlyDictionary<IDofType, int> dofsOfNode = freeDofs.GetDataOfRow(node);
+				if (node.SubdomainsDictionary.Count > 1)
+				{
+					foreach (var dofTypeIdxPair in dofsOfNode)
+					{
+						boundaryDofOrdering[node, dofTypeIdxPair.Key] = subdomainBoundaryIdx++;
+						boundaryToFree.Add(dofTypeIdxPair.Value);
+					}
+				}
+				else
+				{
+					foreach (var dofTypeIdxPair in dofsOfNode)
+					{
+						internalToFree.Add(dofTypeIdxPair.Value);
+					}
+				}
+			}
+
+			return (boundaryDofOrdering, boundaryToFree.ToArray(), internalToFree.ToArray());
+		}
+	}
+}
