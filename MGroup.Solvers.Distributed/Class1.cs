f617abef2d48e60d49bfdbc0232acab7c037e5bc
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2021-05-31 11:52:56 +0300

    Renamed Solvers.Distributed to Solvers.DomainDecomposition and added utility classes.

--- MGroup.Solvers.Distributed/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MGroup.Solvers.Distributed
-{
-    public class Class1
-    {
-    }
-}
---
+++ MGroup.Solvers.DomainDecomposition/LinearAlgebraExtensions/CsrMatrixExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+
+namespace MGroup.Solvers.DomainDecomposition.LinearAlgebraExtensions
+{
+	public static class CsrMatrixExtensions
+	{
+		// TODO: I also need a multithreaded version of this
+		public static double MultiplyRowTimesVector(this CsrMatrix csr, int rowIdx, double[] vector)
+		{
+			double[] values = csr.RawValues;
+			int[] colIndices = csr.RawColIndices;
+			int[] rowOffsets = csr.RawRowOffsets;
+			double dot = 0.0;
+			int start = rowOffsets[rowIdx]; //inclusive
+			int end = rowOffsets[rowIdx + 1]; //exclusive
+			for (int k = start; k < end; ++k) dot += values[k] * vector[colIndices[k]];
+			return dot;
+		}
+	}
+}
